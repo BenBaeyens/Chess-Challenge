f6ffc695fa4d0d4fb7ebe15542d9aa48f9803a27
BenBaeyens <benbaeyens02@gmail.com>
2023-08-16 15:05:37 +0200

    Added highly anticipated "bongcloud" opening to chessbot

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,10 +1,35 @@
 ﻿using ChessChallenge.API;
+using System;
 
 public class MyBot : IChessBot
 {
     public Move Think(Board board, Timer timer)
     {
-        Move[] moves = board.GetLegalMoves();
-        return moves[0];
+
+        Move[] allMoves = board.GetLegalMoves();
+
+        Move moveToPlay = allMoves[0];
+
+        foreach (Move move in allMoves)
+        {
+            // Bongcloud opening
+            if (move.MovePieceType == PieceType.King && (move.TargetSquare.Name == "e7" || move.TargetSquare.Name == "e8" || move.TargetSquare.Name == "e1" || move.TargetSquare.Name == "e2"))
+            {
+                moveToPlay = move;
+                break;
+            }
+
+            // Move the pawn in front of the king
+            else if (move.MovePieceType == PieceType.Pawn && (move.TargetSquare.Name == "e4" || move.TargetSquare.Name == "e5"))
+            {
+                moveToPlay = move;
+                break;
+            }
+        }
+
+        return moveToPlay;
+
+
+
     }
 }
